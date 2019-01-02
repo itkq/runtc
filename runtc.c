07bb0ef9f12d4a1c2e2c6a2597b4489305d8f140
Takuya Kosugiyama <re@itkq.jp>
2019-01-02 23:38:38 +0900

    Change uid and gid in container, just works

--- runtc.c
+++ runtc.c
@@ -1,4 +1,7 @@
 #define _GNU_SOURCE
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <sched.h>
 #include <signal.h>
 #include <stdio.h>
@@ -9,6 +12,12 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+
+#define _PATH_PROC_SETGROUPS "/proc/self/setgroups"
+#define _PATH_PROC_UIDMAP "/proc/self/uid_map"
+#define _PATH_PROC_GIDMAP "/proc/self/gid_map"
+
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
 /* A simple error-handling function: print an error message based
    on the value in 'errno' and terminate the calling process */
@@ -23,26 +32,92 @@ static void usage(char *pname) {
   exit(EXIT_FAILURE);
 }
 
+static inline int write_all(int fd, const void *buf, size_t count) {
+  while (count) {
+    ssize_t tmp;
+
+    errno = 0;
+    tmp = write(fd, buf, count);
+    if (tmp > 0) {
+      count -= tmp;
+      if (count)
+        buf = (const void *)((const char *)buf + tmp);
+    } else if (errno != EINTR && errno != EAGAIN)
+      return -1;
+    if (errno == EAGAIN) /* Try later, *sigh* */
+      usleep(250000);
+  }
+  return 0;
+}
+
+static void map_id(const char *file, u_int32_t from, u_int32_t to) {
+  char *buf;
+  int fd;
+
+  fd = open(file, O_WRONLY);
+  if (fd < 0)
+    errExit("map_id() open");
+
+  asprintf(&buf, "%u %u 1", from, to);
+  if (write_all(fd, buf, strlen(buf)))
+    errExit("write_all");
+  free(buf);
+  close(fd);
+}
+
+enum {
+  SETGROUPS_NONE = -1,
+  SETGROUPS_DENY = 0,
+  SETGROUPS_ALLOW = 1,
+};
+
+static const char *setgroups_strings[] = {[SETGROUPS_DENY] = "deny",
+                                          [SETGROUPS_ALLOW] = "allow"};
+
+static void setgroups_control(int action) {
+  const char *file = _PATH_PROC_SETGROUPS;
+  const char *cmd;
+  int fd;
+
+  if (action < 0 || (size_t)action >= ARRAY_SIZE(setgroups_strings))
+    return;
+  cmd = setgroups_strings[action];
+
+  fd = open(file, O_WRONLY);
+  if (fd < 0) {
+    if (errno == ENOENT)
+      return;
+    errExit("open");
+  }
+
+  if (write_all(fd, cmd, strlen(cmd)))
+    errExit("write_all");
+  close(fd);
+}
+
 int main(int argc, char *argv[]) {
   pid_t pid;
   int status;
-  int unshare_flags = CLONE_NEWUTS | CLONE_NEWNS | CLONE_NEWPID;
+  uid_t real_euid = geteuid();
+  gid_t real_egid = getegid();
+  int unshare_flags = CLONE_NEWCGROUP | CLONE_NEWUSER | CLONE_NEWUTS |
+                      CLONE_NEWNS | CLONE_NEWPID;
   const char *hostname = "runtc";
   const char *domainname = "runtc";
   const char *rootpath = "./root";
+
+  if (argc < 2) {
+    usage(argv[0]);
+  }
+
+  if (-1 == unshare(unshare_flags))
+    errExit("unshare");
 
   if (chroot(rootpath) != 0)
     errExit("chroot");
 
   if (chdir("/"))
     errExit("chdir");
-
-  if (-1 == unshare(unshare_flags))
-    errExit("unshare");
-
-  if (argc < 2) {
-    usage(argv[0]);
-  }
 
   pid = fork();
   switch (pid) {
@@ -51,6 +126,7 @@ int main(int argc, char *argv[]) {
   case 0: // child
     break;
   default: // parent
+
     if (waitpid(pid, &status, 0) == -1)
       errExit("waitpid");
 
@@ -64,6 +140,14 @@ int main(int argc, char *argv[]) {
   if (mount("proc", "/proc", "proc", MS_NOSUID | MS_NOEXEC | MS_NODEV, NULL) !=
       0)
     errExit("mount");
+
+  /* since Linux 3.19 unprivileged writing of /proc/self/gid_map
+   * has s been disabled unless /proc/self/setgroups is written
+   * first to permanently disable the ability to call setgroups
+   * in that user namespace. */
+  setgroups_control(SETGROUPS_DENY);
+  map_id(_PATH_PROC_UIDMAP, 0, real_euid);
+  map_id(_PATH_PROC_GIDMAP, 0, real_egid);
 
   if (sethostname(hostname, strlen(hostname)) != 0)
     errExit("sethostname");
